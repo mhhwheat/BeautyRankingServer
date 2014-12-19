4e46ef1aab09d55981a0744b1642b28116c80695
mhhwheat <1057251411@qq.com>
2014-12-19 16:25:21 +0800

    modified Comment entity

--- src/org/wheat/beautyRanking/entity/Comment.java
+++ src/org/wheat/beautyRanking/entity/Comment.java
@@ -2,82 +2,92 @@ package org.wheat.beautyRanking.entity;
 
 import java.util.Date;
 
-import org.wheat.beautyRanking.data.DataType;
-import org.wheat.beautyRanking.data.IData;
-
 import com.google.gson.annotations.SerializedName;
 
 
-public class Comment implements IData
+public class Comment
 {
-	private static final long serialVersionUID = 1L;
-	private int dataType=DataType.ENTITY_COMMENT;
 	//照片ID
 	@SerializedName("photoId")
 	private int photoID;
-	//mmid
-	@SerializedName("beautyId")
-	private int beautyID;
+	
 	//用户手机号
 	@SerializedName("userPhoneNumber")
 	private String userPhoneNumber;
+	
+	//用户头像地址
+	@SerializedName("userAvatar")
+	private String userAvatar;
+	
+	//用户昵称
+	@SerializedName("userNickName")
+	private String userNickName;
+	
 	//评论时间
 	@SerializedName("commentTime")
 	private Date commentTime;
+	
 	//评论的内容
 	@SerializedName("commentContent")
 	private String commentContent;
-	public Comment(int photoID,int beautyID,String userPhoneNumber,Date commentTime,String commentContent)
-	{
-		this.photoID=photoID;
-		this.beautyID=beautyID;
-		this.userPhoneNumber=userPhoneNumber;
-		this.commentTime=commentTime;
-		this.commentContent=commentContent;
-	}
+	
 	public void setPhotoID(int photoID)
 	{
 		this.photoID=photoID;
 	}
+	
 	public int getPhotoID()
 	{
 		return photoID;
 	}
-	public void setBeautyID(int beautyID)
-	{
-		this.beautyID=beautyID;
-	}
-	public int getBeautyID()
-	{
-		return this.beautyID;
-	}
+	
 	public void setUserPhoneNumber(String userPhoneNumber)
 	{
 		this.userPhoneNumber=userPhoneNumber;
 	}
+	
 	public String getUserPhoneNumber()
 	{
 		return this.userPhoneNumber;
 	}
+	
+	public void setUserAvatar(String userAvatar)
+	{
+		this.userAvatar=userAvatar;
+	}
+	
+	public String getUserAvatar()
+	{
+		return this.userAvatar;
+	}
+	
+	public void setUserNickName(String userNickName)
+	{
+		this.userNickName=userNickName;
+	}
+	
+	public String getUserNickName()
+	{
+		return this.userNickName;
+	}
+	
 	public void setCommentTime(Date commentTime)
 	{
 		this.commentTime=commentTime;
 	}
+	
 	public Date getCommentTime()
 	{
 		return this.commentTime;
 	}
+	
 	public void setCommentContent(String commentContent)
 	{
 		this.commentContent=commentContent;
 	}
+	
 	public String getCommentContent()
 	{
 		return this.commentContent;
-	}
-	@Override
-	public int getDataType() 
-	{
-		return dataType;
 	}
 }
